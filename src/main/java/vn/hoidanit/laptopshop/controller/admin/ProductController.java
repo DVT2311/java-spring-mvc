ad25e70224c4dcb05327b635f26f7059afab34f0
Tam KeyBoard <dinhvantam333@gmail.com>
2025-06-16 16:32:12 +0700

    video 91

--- src/main/java/vn/hoidanit/laptopshop/controller/admin/ProductController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/admin/ProductController.java
@@ -4,9 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class DashboardController {
-    @GetMapping("/admin")
+public class ProductController {
+    @GetMapping("/admin/product")
     public String getDashboard() {
-        return "admin/dashboard/show";
+        return "admin/product/show";
     }
 }
